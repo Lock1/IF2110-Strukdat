965c843d2aa720526243b4de7e1f9308ed1e0918
nypnop <68521696+nypnop@users.noreply.github.com>
2020-11-03 18:35:24 +0700

    Update jam.h

--- src/structure/jam.h
+++ src/structure/jam.h
@@ -7,7 +7,7 @@
 
 #include "boolean.h"
 
-/* *** Definisi TYPE JAM <HH:MM:SS> *** */
+/* *** Definisi TYPE JAM <HH:MM> *** */
 typedef struct {
 	int HH; /* integer [0..23] */
 	int MM; /* integer [0..59] */
@@ -23,13 +23,13 @@ typedef struct {
 /* KELOMPOK VALIDASI TERHADAP TYPE                                   */
 /* ***************************************************************** */
 boolean IsJAMValid (int H, int M);
-/* Mengirim true  jika H,M,S dapat membentuk J yang valid */
+/* Mengirim true  jika H,M dapat membentuk J yang valid */
 /* dipakai untuk mentest SEBELUM membentuk sebuah Jam */
 
 /* *** Konstruktor: Membentuk sebuah JAM dari komponen-komponennya *** */
 JAM MakeJAM (int HH, int MM);
 /* Membentuk sebuah JAM dari komponen-komponennya yang valid */
-/* Prekondisi : HH, MM, SS valid untuk membentuk JAM */
+/* Prekondisi : HH, MM valid untuk membentuk JAM */
 
 /* ***************************************************************** */
 /* KELOMPOK BACA/TULIS                                               */
@@ -37,60 +37,49 @@ JAM MakeJAM (int HH, int MM);
 void BacaJAM (JAM * J);
 /* I.S. : J tidak terdefinisi */
 /* F.S. : J terdefinisi dan merupakan jam yang valid */
-/* Proses : mengulangi membaca komponen HH, MM, SS sehingga membentuk J */
+/* Proses : mengulangi membaca komponen HH, MM sehingga membentuk J */
 /* yang valid. Tidak mungkin menghasilkan J yang tidak valid. */
-/* Pembacaan dilakukan dengan mengetikkan komponen HH, MM, SS
+/* Pembacaan dilakukan dengan mengetikkan komponen HH, MM.
    dalam satu baris, masing-masing dipisahkan 1 spasi, diakhiri enter. */
 /* Jika JAM tidak valid maka diberikan pesan: "Jam tidak valid", dan pembacaan
    diulangi hingga didapatkan jam yang valid. */
 /* Contoh:
-   60 3 4
+   60 3
    Jam tidak valid
-   1 3 4
-   --> akan terbentuk JAM <1,3,4> */
+   1 3
+   --> akan terbentuk JAM <1,3> */
 
 void TulisJAM (JAM J);
 /* I.S. : J sembarang */
-/* F.S. :   Nilai J ditulis dg format HH:MM:SS */
-/* Proses : menulis nilai setiap komponen J ke layar dalam format HH:MM:SS
-   tanpa karakter apa pun di depan atau belakangnya, termasuk spasi, enter, dll.
-   Jika jam / menit / detik hanya satu digit, tuliskan satu digit tanpa 0 di depannya. */
+/* F.S. :   Nilai J ditulis dg format HH hour (s) MM minute (s) jika minute tidak 0.*/
 
 /* ***************************************************************** */
 /* KELOMPOK KONVERSI TERHADAP TYPE                                   */
 /* ***************************************************************** */
 long JAMToMenit (JAM J);
-/* Diberikan sebuah JAM, mengkonversi menjadi jumlah detik dari pukul 0:0:0 */
-/* Rumus : detik = 3600*HH + 60*MM + SS */
-/* Nilai maksimum = 3600*23+59*60+59 */
+/* Diberikan sebuah JAM, mengkonversi menjadi jumlah menit*/
+/* Rumus : menit = 60*HH + MM */
+/* Nilai maksimum = 60*23+59 */
 
 JAM MenitToJAM (long N);
 /* Mengirim  konversi detik ke JAM */
-/* Catatan: Jika N >= 86400, maka harus dikonversi dulu menjadi jumlah detik yang
-   mewakili jumlah detik yang mungkin dalam 1 hari, yaitu dengan rumus:
-   N1 = N mod 86400, baru N1 dikonversi menjadi JAM */
+/* Catatan: Jika N >= 1440, maka harus dikonversi dulu menjadi jumlah menit yang
+   mewakili jumlah menit yang mungkin dalam 1 hari, yaitu dengan rumus:
+   N1 = N mod 1440, baru N1 dikonversi menjadi JAM */
 
 /* ***************************************************************** */
 /* KELOMPOK OPERASI TERHADAP TYPE                                    */
 /* ***************************************************************** */
-/* *** Kelompok Operator Relational *** */
-boolean JEQ (JAM J1, JAM J2);
-/* Mengirimkan true jika J1=J2, false jika tidak */
-boolean JNEQ (JAM J1, JAM J2);
-/* Mengirimkan true jika J1 tidak sama dengan J2 */
-boolean JLT (JAM J1, JAM J2);
-/* Mengirimkan true jika J1<J2, false jika tidak */
-boolean JGT (JAM J1, JAM J2);
-/* Mengirimkan true jika J1>J2, false jika tidak */
+
 /* *** Operator aritmatika JAM *** */
 
-JAM NextNDetik (JAM J, int N);
-/* Mengirim N detik setelah J dalam bentuk JAM */
+JAM NextNMenit (JAM J, int N);
+/* Mengirim N menit setelah J dalam bentuk JAM */
 
 
 /* *** Kelompok Operator Aritmetika *** */
 long Durasi (JAM JAw, JAM JAkh);
-/* Mengirim JAkh-JAw dlm Detik, dengan kalkulasi */
+/* Mengirim JAkh-JAw dlm menit, dengan kalkulasi */
 /* Jika JAw > JAkh, maka JAkh adalah 1 hari setelah JAw */
 
 #endif
