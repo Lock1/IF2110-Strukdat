8d14f49a29cf22db6004009708b36069816e1f32
fwznbg <fnaabigh@gmail.com>
2020-11-06 10:38:33 +0700

    add ADT stack

--- src/structure/stack.c
+++ src/structure/stack.c
@@ -1,22 +1,33 @@
 #include "stack.h"
 
+
+
+/* Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 void CreateEmpty (Stack *S){
     Top(*S) = Nil;
 }
 
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
 boolean IsEmpty (Stack S){
     return Top(S) == Nil;
 }
 
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
 boolean IsFull (Stack S){
     return Top(S) == MaxEl-1;  
 }
 
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+
+/* Menambahkan X sebagai elemen Stack S. */
 void Push (Stack * S, infotype X){
     Top(*S)++;
     InfoTop(*S) = X;
 }
 
+/* Menghapus X dari Stack S. */
 void Pop (Stack * S, infotype* X){
     *X = InfoTop(*S);
     Top(*S)--;
