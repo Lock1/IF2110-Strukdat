862148affcb745022b18bc982f8e28513b4920c7
Lock1 <rizaltanur@outlook.com>
2020-11-19 15:07:53 +0700

    Perubahan lokasi driver file io
    Perubahan lokasi driver dan struktur material dan wahana

--- src/driver-fileio.c
+++ src/driver-fileio.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "fileio.h"
+#include "../../fileio.h"
 
 int main(){
 	FILE* file=fopen("wahana.txt", "r");
 	if (!file)
 		return 1;
-	
+
 	char buffer[200];
 	fgets(buffer, 200, file);
-	
+
 	while(!feof(file)){
 		Wahana w;
 		sscanf(buffer,"%d %s %d %d %d %s", &w.ID, w.nama, &w.harga, &w.durasi, &w.kapasitas, w.deskripsi);
@@ -18,6 +18,6 @@ int main(){
 		printf("%s\n", w.nama);
 	}
 	fclose(file);
-	
+
 	return 0;
 }
