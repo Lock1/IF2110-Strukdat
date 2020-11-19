75aed4137b45f1820235ba8deee2ee49583e933c
fadelananda <fadelananda123@gmail.com>
2020-11-20 00:15:36 +0700

    material.h

--- src/structure/material.h
+++ src/structure/material.h
@@ -1,4 +1,20 @@
+//13519146 - Fadel Ananda Dotty
+//13519110 - M. Akromi Afif
+
+#ifndef _MATERIAL_H_
+#define _MATERIAL_H_
+
 typedef struct Material{
 	int ID, harga;
 	char nama[999];
+	char gambar;
 }Material;
+
+#define ID(M) (M).ID
+#define Harga(M) (M).harga
+#define Nama(M) (M).nama
+#define Gambar(M) (M).gambar
+
+
+
+#endif
