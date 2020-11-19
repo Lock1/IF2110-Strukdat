3da22887ca9d7774df9f70d69702055d98721c53
fadelananda <fadelananda123@gmail.com>
2020-11-19 14:18:17 +0700

    add fileio 1

---
+++ src/fileio.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "fileio.h"
+
+int main(){
+	FILE* file=fopen("wahana.txt", "r");
+	if (!file)
+		return 1;
+	
+	char buffer[200];
+	fgets(buffer, 200, file);
+	
+	while(!feof(file)){
+		Wahana w;
+		sscanf(buffer,"%d %s %d %d %d %s", &w.ID, w.nama, &w.harga, &w.durasi, &w.kapasitas, w.deskripsi);
+		printf("read wahana: %d %s %d %d %d %s\n", w.ID, w.nama, w.harga, w.durasi, w.kapasitas, w.deskripsi);
+		fgets(buffer, 200, file);
+		printf("%s\n", w.nama);
+	}
+	fclose(file);
+	
+	return 0;
+}
---
+++ src/fileio.h
@@ -0,0 +1,5 @@
+typedef struct Wahana{
+	int ID, harga, durasi, kapasitas;
+	char nama[999];
+	char deskripsi[999];
+}Wahana;
