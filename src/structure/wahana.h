fec82485cde7ad18c5121bb12206d1e538c8c543
Akromi Afif <socakromiafif@gmail.com>
2020-11-19 16:04:49 +0700

    add wahana

--- src/structure/wahana.h
+++ src/structure/wahana.h
@@ -1,6 +1,15 @@
-typedef struct Wahana{
+#ifndef _WAHANA_H_
+#define _WAHANA_H_
+
+#include "boolean.h"
+
+typedef struct {
 	int ID, harga, durasi, kapasitas;
 	char nama[999];
 	char deskripsi[999];
-    char gambar[999];
-}Wahana;
+  char gambar[999];
+} Wahana;
+
+Wahana MakeWahana(int ID, int harga, int durasi, int kapasitas, char nama, char deskripsi, char gambar);
+
+#endif
