bd4f8884d0e512c8ed1225e333931a0865c98f90
fadelananda <fadelananda123@gmail.com>
2020-11-20 16:39:22 +0700

    config for bintree

--- src/fileio.h
+++ src/fileio.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "structure/wahana.h"
 #include "structure/material.h"
+#include "structure/tree.h"
 
 int ReadFromWahana(Wahana** database);
 int ReadFromBahan(Material** database);
